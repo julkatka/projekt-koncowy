8f6399335ac72f084b94ffc27cd8187d0cc5eaba
Julia <jtchorzewska@gmail.com>
2021-04-24 12:39:40 +0200

    wyświetlenie 1 statku

---
+++ space-app/src/app/space/space.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SpaceRoutingModule } from './space-routing.module';
+import { HangarComponent } from './hangar/hangar.component';
+import { SpaceShipsComponent } from './space-ships/space-ships.component';
+
+
+@NgModule({
+  declarations: [
+    HangarComponent,
+    SpaceShipsComponent
+  ],
+  exports: [
+    HangarComponent
+  ],
+  imports: [
+    CommonModule,
+    SpaceRoutingModule
+  ]
+})
+export class SpaceModule { }
