9274594f76f4db1a375b642920753633c391d387
Julia <jtchorzewska@gmail.com>
2021-04-24 23:05:51 +0200

    statek nr 2 - wyswietlenie

--- space-app/src/app/space/space.module.ts
+++ space-app/src/app/space/space.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SpaceRoutingModule } from './space-routing.module';
 import { HangarComponent } from './hangar/hangar.component';
 import { SpaceShipsComponent } from './space-ships/space-ships.component';
+import { SpaceShips2Component } from './space-ships2/space-ships2.component';
 
 
 @NgModule({
   declarations: [
     HangarComponent,
-    SpaceShipsComponent
+    SpaceShipsComponent,
+    SpaceShips2Component
   ],
   exports: [
     HangarComponent
